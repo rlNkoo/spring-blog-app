529284e2a98981d1ea1e807ba7ab11618e7fc1af
rlNkoo <rolakartur96@gmail.com>
2024-04-04 20:36:04 +0200

    First commit

---
+++ src/main/java/pl/rlnkoo/blogappspringboot/BlogAppSpringBootApplication.java
@@ -0,0 +1,13 @@
+package pl.rlnkoo.blogappspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogAppSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlogAppSpringBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/rlnkoo/blogappspringboot/BlogAppSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.rlnkoo.blogappspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogAppSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
